c96ba937d5773f989437b8d1a6642533e814b0c1
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-06 01:21:18 +0300

    Bash Tr command practices

---
+++ hackerrank_practices/25_tr_1.sh
@@ -0,0 +1,4 @@
+# In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+# In a given fragment of text, replace all parentheses   with box brackets  .
+
+tr "()" "[]"
---
+++ hackerrank_practices/26_tr_2.sh
@@ -0,0 +1,5 @@
+# In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+
+# In a given fragment of text, delete all the lowercase characters a - z .
+
+tr -d a-z
