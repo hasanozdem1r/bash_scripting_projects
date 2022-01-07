5cc4e462b0d0c7555303bbce0ec3d333774fb5a6
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-07 21:27:44 +0300

    Bash sort practices

---
+++ hackerrank_practices/32_sort_5.sh
@@ -0,0 +1,7 @@
+# You are given a file of text,which contains temperature information about American cities, in TSV (tab-separated) format. 
+# The first column is the name of the city and the next four columns are 
+#the average temperature in the months of Jan, Feb, March and April (see the sample input). 
+#and April (see the sample input). Rearrange the rows of the table in descending order of the values for the average 
+#temperature in January.
+
+sort -t $'\t' -k2,2 -n -r
