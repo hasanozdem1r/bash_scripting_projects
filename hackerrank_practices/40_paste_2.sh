2e56bb4a7ead5fcb858bf3a0823ca28957b11109
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-12 17:36:15 +0300

    Paste command practice

---
+++ hackerrank_practices/40_paste_2.sh
@@ -0,0 +1,5 @@
+#In this challenge, we practice using the paste command to merge lines of a given file.
+#You are given a CSV file where each row contains the name of a city and its state separated by a comma. 
+#Your task is to restructure the file so that three consecutive rows are folded into one line and are separated by semicolons.
+
+paste -d';' - - -
