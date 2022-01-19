166578500502562b585d82b45d8a831a163bd515
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-19 20:16:19 +0300

    sed command practices

---
+++ hackerrank_practices/62_sed_5.sh
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/sed-command-5/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+sed 's/\([[:digit:]]\{4\}\) \([[:digit:]]\{4\}\) \([[:digit:]]\{4\}\) \([[:digit:]]\{4\}\)/\4 \3 \2 \1/'
