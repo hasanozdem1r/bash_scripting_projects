4e488c9061d213b09ef92d649bb1af7c0a60d1ca
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2023-06-12 23:57:43 +0300

    feature: processes are automated

---
+++ projects/clean_temp_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf "C:\\Windows\\Temp\\"
+rm -rf "${HOME}\\AppData\\Local\\Temp"
+rm -rf "C:\\Windows\\Prefetch"
+
---
+++ projects/generate_ssh_key.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set the SSH key path
+ssh_key_path=~/.ssh/id_ed25519
+
+# Generate a new SSH key
+yes | ssh-keygen -t ed25519 -C "hasanozdemir1@trakya.edu.tr" -f "$ssh_key_path"
+# Start the SSH agent
+eval "$(ssh-agent -s)"
+
+# Add the SSH private key to the agent
+ssh-add "$ssh_key_path"
+
+# Display the SSH public key
+cat "$ssh_key_path.pub"
